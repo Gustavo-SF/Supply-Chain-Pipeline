09f5477fc0f7967c0912a8aecf47d2599cd306d1
Gustavo-SF <g.s.fonseca32@gmail.com>
2021-05-04 16:52:11 +0100

    cleaned up deployment files

---
+++ deployment.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+echo "Starting the deployment..."
+
+# initial variables
+resourceGroup="DataScienceThesisRG"
+location="westeurope"
+storageAccount="dsthesissa"
+
+# create the resource group
+az group create -n $resourceGroup -l $location
+
+# set up the --defaults in az configure
+az configure --defaults group=$resourceGroup
+az configure --defaults location=$location
+
+# create a storage account
+az storage account create \
+	-n $storageAccount \
+	--sku Standard_LRS \
+	--hns true  # ADLS setting
+
+
+storageID=$(az storage account show -n $storageAccount --query id -o tsv)
+
+# setting up Azure AD for Blob and Queue storage
+az ad signed-in-user show --query objectId -o tsv | az role assignment create --role "Storage Blob Data Contributor" --assignee @- --scope $storageID
+az ad signed-in-user show --query objectId -o tsv | az role assignment create --role "Storage Queue Data Contributor" --assignee @- --scope $storageID
+
+sleep 5m
+
+# setting up AZCopy
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -xvf downloadazcopy-v10-linux
+mv ./azcopy_linux_amd64_*/azcopy .
+rm -r downloadazcopy-v10-linux ./azcopy_linux_amd64_*
+
+# create a container / file system
+az storage fs create \
+	-n raw-data \
+	--auth-mode login \
+	--account-name $storageAccount
+
+az storage fs create \
+	-n data-ready \
+	--auth-mode login \
+	--account-name $storageAccount
+
+
+echo "Uploading files to storage"
+
+# sas code setting up to transfer files
+end=$(date -u -d "200 minutes" '+%Y-%m-%dT%H:%MZ')
+sas=$(az storage account generate-sas --permissions cdlruwap --account-name $storageAccount --services b --resource-types sco --expiry $end -o tsv)
+
+# copy all files from raw-data/ folder
+./azcopy copy "data/raw-data/*" "https://${storageAccount}.dfs.core.windows.net/raw-data/?${sas}" --recursive
+
+echo "Now for the Queues..."
+
+# setting up the queue storage
+queueName="uploadedfiles"
+
+# create Queue
+az storage queue create \
+	--name $queueName \
+	--account-name $storageAccount \
+	--auth-mode login
+
+cd ConvertData/
+
+# run the Azure FunctionApp deployment script
+bash deploy_script.sh
+
+echo "Creating and Populating SQL Database"
+
+# SQL DATABASE
+cd ../Deploy-Azure-DB
+
+# run the Azure SQL DB deployment script
+bash deploy_script.sh
+
+echo "Completed!"
+
+
---
+++ maintenance.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+resourceGroup="DataScienceThesisRG"
+location="westeurope"
+storageAccount="dsthesissa"
+
+# save the previous data in another folder
+az storage fs create -n old-ready --auth-mode login --account-name $storageAccount
+
+# get the secrets
+source Deploy-Azure-DB/secrets/database_pws.sh
+server=$(az sql server list --query [0].name -o tsv)
+
+# get ip and open firewall to it
+thisIP=$(wget -O - -q https://icanhazip.com/)
+az sql server firewall-rule create \
+    --server $server \
+    -n AllowMyIP \
+    --start-ip-address $thisIP \
+    --end-ip-address $thisIP
+
+# get the last date in the data and extract month and year
+lastentry=$(sqlcmd -S tcp:$server.database.windows.net -d $database -U $login -P $password -Q "SELECT MAX(EntryDate) FROM [PPP].[MB51]" | awk '/2/ {print $1}')
+yearLastEntry=$(echo $lastentry | awk 'BEGIN { FS = "-" } ; { print $1 }')
+monthLastEntry=$(echo $lastentry | awk 'BEGIN { FS = "-" } ; { print $2 }')
+
+# setting up AZCopy
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -xvf downloadazcopy-v10-linux
+mv ./azcopy_linux_amd64_*/azcopy .
+rm -r downloadazcopy-v10-linux ./azcopy_linux_amd64_*
+
+echo "Uploading files to storage"
+
+# setting up the sas code
+end=$(date -u -d "200 minutes" '+%Y-%m-%dT%H:%MZ')
+sas=$(az storage account generate-sas --permissions cdlruwap --account-name $storageAccount --services b --resource-types sco --expiry $end -o tsv)
+
+# Needs to transfer with blob identification. DFS doesn't seem to be working.
+# Move and delete previous files
+./azcopy copy "https://${storageAccount}.blob.core.windows.net/data-ready/*?${sas}" "https://${storageAccount}.blob.core.windows.net/old-ready/${lastentry}/data-ready/?${sas}" --recursive
+./azcopy copy "https://${storageAccount}.blob.core.windows.net/raw-data/*?${sas}" "https://${storageAccount}.blob.core.windows.net/old-ready/${lastentry}/raw-data/?${sas}" --recursive
+./azcopy rm "https://${storageAccount}.blob.core.windows.net/data-ready/*?${sas}" --recursive
+./azcopy rm "https://${storageAccount}.blob.core.windows.net/raw-data/*?${sas}" --recursive
+
+# need to check if rows with headers are correct in the Function App code
+./azcopy copy "Data-Preparation/raw-data/*" "https://${storageAccount}.dfs.core.windows.net/raw-data/?${sas}"  --recursive
+
+
+cd ConvertData/
+
+# run the Azure FunctionApp deployment script
+bash/maintenance_script.sh
+
+cd ../Deploy-Azure-DB/
+
+# run the Azure SQL DB deployment script
+bash/maintenance_script.sh
+
+echo "Completed!"
